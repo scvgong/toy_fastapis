5b94c9824f72ba554fb8e42ece034b6c51ff6c22
scvgong <gongmyeongyun@gmail.com>
2024-01-05 13:36:19 +0900

    modifiy

--- routes/users.py
+++ routes/users.py
@@ -55,6 +55,9 @@ async def list(request:Request):
 # database = mongoClient["toy_fastapis"]
 # # collection 작업
 # collection = database['users']
+from databases.connections import Database
+from models.users import User
+collection_user = Database(User)
 
 @router.get("/list") # 펑션 호출 방식
 async def list(request:Request):
@@ -70,7 +73,7 @@ async def list(request:Request):
     # insert 작업 진행
     # documents = collection.find({})
     # # cast cursor to list
-    user_list = []
+    user_list = await collection_user.get_all()
 
     # for document in documents: 
     #     # print("document : {}".format(document))
@@ -80,12 +83,15 @@ async def list(request:Request):
     # return templates.TemplateResponse(name="users/list.html"
     return templates.TemplateResponse(name="users/list_jinja.html", context={'request':request,'users': user_list})
 
+from beanie import PydanticObjectId
+
 # 회원 상세정보 /users/read -> users/reads.html
 # Path parameters : /users/read/id or /users/read/uniqe_name
 @router.get("/read/{object_id}") # 펑션 호출 방식
-async def reads(request:Request, object_id):
+async def reads(request:Request, object_id:PydanticObjectId):
     print(dict(request._query_params))
-    return templates.TemplateResponse(name="users/reads.html", context={'request':request})
+    user = await collection_user.get(object_id)
+    return templates.TemplateResponse(name="users/reads.html", context={'request':request,'user':user})
 
 @router.post("/read/{object_id}") # 펑션 호출 방식
 async def reads(request:Request, object_id):
